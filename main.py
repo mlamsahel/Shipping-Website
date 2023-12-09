8c17fb2f40a6d79e8635041751e3e58b392955da
AnBloop <jimmyredmond15@gmail.com>
2023-12-09 17:42:30 -0600

    Removed all the chatgpt stuff Removed the gui.py file (was frustrating) Simplified everything in an unsimple way this is disgusting, but it works

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from gui import *
 import psycopg2
+import tkinter as gui
+
+
 
 # Replace these with your PostgreSQL connection details
 host = '192.168.1.42'  # Replace with your PostgreSQL server hostname or IP address
@@ -7,6 +9,7 @@ database_name = 'postgres'  # Replace with your database name
 user = 'postgres'  # Replace with your PostgreSQL username
 password = '06896#Qz1'  # Replace with your PostgreSQL password
 
+    
 
 database = psycopg2.connect(
     dbname=database_name,
@@ -19,10 +22,67 @@ def execute(input): #simplifying the cursor.exeucte command into a simple method
     
     cursor = database.cursor()
     cursor.execute(input)
-    outp = cursor.fetchall()
+    try:
+        outp = cursor.fetchall()
+    except Exception as e:
+        print(e)
+        outp = "No output"
     database.commit()
     return outp
 
-createGUI()
+def createGUI():
+    global source
+    source = gui.Tk()
+    source.title("Amazon't Database")
+    source.geometry("1000x800")
 
-database.close()
+    addCustomerButton = gui.Button(source, text="Add Customer", command=openAddCustomerPrompt)
+    addCustomerButton.pack()
+
+    source.mainloop()
+
+def openAddCustomerPrompt():
+    
+    addCustomerPromptWindow = gui.Toplevel()
+    addCustomerPromptWindow.title("Add Customer")
+    addCustomerPromptWindow.geometry("300x150")
+
+    def submitCustomer():
+        cID = CustomerIDBox.get()
+        cFN = CustomerFNameBox.get()
+        cLN = CustomerLNameBox.get()
+        cAD = CustomerAdrBox.get()
+
+        try:
+            print("A")
+            outp = execute(f"insert into customer values('{cID}','{cFN}','{cLN}','{cAD}');")
+            print("B")
+            print(outp)
+            print("C")
+            addCustomerPromptWindow.destroy()
+            print("D")
+            
+        except Exception as e:
+            
+            print(e)
+
+    CustomerIDBox = gui.Entry(addCustomerPromptWindow)
+    CustomerFNameBox = gui.Entry(addCustomerPromptWindow)
+    CustomerLNameBox = gui.Entry(addCustomerPromptWindow)
+    CustomerAdrBox = gui.Entry(addCustomerPromptWindow)
+    CustomerSubmit = gui.Button(addCustomerPromptWindow, text="Submit", command=submitCustomer)
+
+    CustomerIDBox.insert(0, "Enter ID")
+    CustomerFNameBox.insert(0, "Enter First Name")
+    CustomerLNameBox.insert(0, "Enter Last Name")
+    CustomerAdrBox.insert(0, "Enter Address")
+
+    CustomerIDBox.pack()
+    CustomerFNameBox.pack()
+    CustomerLNameBox.pack()
+    CustomerAdrBox.pack()
+    CustomerSubmit.pack()
+
+
+
+createGUI()
